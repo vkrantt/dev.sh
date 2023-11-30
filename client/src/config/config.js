e02b40499607b4d922fa9841ddf4b301eb393394
vkrantt <kr.vikrantkumar@gmail.com>
2023-11-30 20:04:04 +0530

    Added server url

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:3030/api";
+// export const BASE_URL = "http://localhost:3030/api";
+export const BASE_URL = "https://dev-sh-hwg0.onrender.com";
 
 export const toastConfig = {
   duration: 2000,
